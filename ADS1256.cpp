bf51d255d7529b0cc693e113fec2f9aa76a18ece
Axel <ansepulveda@uc.cl>
2020-04-14 15:59:39 -0400

    add readCurrentChannelRaw() to cpp

--- ADS1256.cpp
+++ ADS1256.cpp
@@ -93,6 +93,15 @@ float ADS1256::readCurrentChannel() {
          _conversionFactor;
 }
 
+float ADS1256::readCurrentChannelRaw() {
+  CSON();
+  SPI.transfer(RDATA);
+  __builtin_avr_delay_cycles(200);  // t6 delay
+  float adsCode = read_float32();
+  CSOFF();
+  return ((adsCode / 0x7FFFFF) * _conversionFactor;
+}
+
 // Call this ONLY after RDATA command
 unsigned long ADS1256::read_uint24() {
   unsigned char _highByte, _midByte, _lowByte;
